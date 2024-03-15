3d4aba33bf88a3aa0b717ffc0cf00931f90dc942
Daute Rodríguez <dauter@ext.inditex.com>
2024-03-15 16:14:14 +0000

    nested test

--- src/test/java/org/example/MarsRoverTest.java
+++ src/test/java/org/example/MarsRoverTest.java
@@ -1,12 +1,14 @@
 package org.example;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MarsRoverTest {
-    
+
+
     @Test
     void spawnsAtOriginFacingNorth() {
         String command = "";
@@ -19,100 +21,103 @@ class MarsRoverTest {
         assertThat(expectedOutput).isEqualTo(output);
     }
 
-    @Test
-    void rotatesLeft() {
-        String command = "L";
-        String expectedOutput = "0:0:W";
+    @Nested
+    class RotateTest {
+        @Test
+        void rotatesLeft() {
+            String command = "L";
+            String expectedOutput = "0:0:W";
 
-        MarsRover marsRover = new MarsRover(new Grid());
+            MarsRover marsRover = new MarsRover(new Grid());
 
-        String output = marsRover.execute(command);
+            String output = marsRover.execute(command);
 
-        assertThat(expectedOutput).isEqualTo(output);
-    }
+            assertThat(expectedOutput).isEqualTo(output);
+        }
 
-    @Test
-    void turnsAroundToTheLeft() {
-        String command = "LL";
-        String expectedOutput = "0:0:S";
+        @Test
+        void turnsAroundToTheLeft() {
+            String command = "LL";
+            String expectedOutput = "0:0:S";
 
-        MarsRover marsRover = new MarsRover(new Grid());
+            MarsRover marsRover = new MarsRover(new Grid());
 
-        String output = marsRover.execute(command);
+            String output = marsRover.execute(command);
 
-        assertThat(expectedOutput).isEqualTo(output);
-    }
+            assertThat(expectedOutput).isEqualTo(output);
+        }
 
-    @Test
-    void rotatesLeftThreeTimes() {
-        String command = "LLL";
-        String expectedOutput = "0:0:E";
+        @Test
+        void rotatesLeftThreeTimes() {
+            String command = "LLL";
+            String expectedOutput = "0:0:E";
 
-        MarsRover marsRover = new MarsRover(new Grid());
+            MarsRover marsRover = new MarsRover(new Grid());
 
-        String output = marsRover.execute(command);
+            String output = marsRover.execute(command);
 
-        assertThat(expectedOutput).isEqualTo(output);
-    }
+            assertThat(expectedOutput).isEqualTo(output);
+        }
 
-    @Test
-    void rotates360ToTheLeft() {
-        String command = "LLLL";
-        String expectedOutput = "0:0:N";
+        @Test
+        void rotates360ToTheLeft() {
+            String command = "LLLL";
+            String expectedOutput = "0:0:N";
 
-        MarsRover marsRover = new MarsRover(new Grid());
+            MarsRover marsRover = new MarsRover(new Grid());
 
-        String output = marsRover.execute(command);
+            String output = marsRover.execute(command);
 
-        assertThat(expectedOutput).isEqualTo(output);
-    }
+            assertThat(expectedOutput).isEqualTo(output);
+        }
 
-    @Test
-    void rotatesRight() {
-        String command = "R";
-        String expectedOutput = "0:0:E";
+        @Test
+        void rotatesRight() {
+            String command = "R";
+            String expectedOutput = "0:0:E";
 
-        MarsRover marsRover = new MarsRover(new Grid());
+            MarsRover marsRover = new MarsRover(new Grid());
 
-        String output = marsRover.execute(command);
+            String output = marsRover.execute(command);
 
-        assertThat(expectedOutput).isEqualTo(output);
-    }
+            assertThat(expectedOutput).isEqualTo(output);
+        }
 
-    @Test
-    void turnsAroundToTheRight() {
-        String command = "RR";
-        String expectedOutput = "0:0:S";
+        @Test
+        void turnsAroundToTheRight() {
+            String command = "RR";
+            String expectedOutput = "0:0:S";
 
-        MarsRover marsRover = new MarsRover(new Grid());
+            MarsRover marsRover = new MarsRover(new Grid());
 
-        String output = marsRover.execute(command);
+            String output = marsRover.execute(command);
 
-        assertThat(expectedOutput).isEqualTo(output);
-    }
+            assertThat(expectedOutput).isEqualTo(output);
+        }
 
-    @Test
-    void rotatesRightThreeTimes() {
-        String command = "RRR";
-        String expectedOutput = "0:0:W";
+        @Test
+        void rotatesRightThreeTimes() {
+            String command = "RRR";
+            String expectedOutput = "0:0:W";
 
-        MarsRover marsRover = new MarsRover(new Grid());
+            MarsRover marsRover = new MarsRover(new Grid());
 
-        String output = marsRover.execute(command);
+            String output = marsRover.execute(command);
 
-        assertThat(expectedOutput).isEqualTo(output);
-    }
+            assertThat(expectedOutput).isEqualTo(output);
+        }
 
-    @Test
-    void rotates360ToTheRight() {
-        String command = "RRRR";
-        String expectedOutput = "0:0:N";
+        @Test
+        void rotates360ToTheRight() {
+            String command = "RRRR";
+            String expectedOutput = "0:0:N";
 
-        MarsRover marsRover = new MarsRover(new Grid());
+            MarsRover marsRover = new MarsRover(new Grid());
 
-        String output = marsRover.execute(command);
+            String output = marsRover.execute(command);
 
-        assertThat(expectedOutput).isEqualTo(output);
+            assertThat(expectedOutput).isEqualTo(output);
+        }
     }
 
 }
